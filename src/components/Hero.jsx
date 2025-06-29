5890d1a8d7c73f523a3228aee24ad2855eb33745
moatasim <mo32000a@gmail.com>
2025-06-29 20:04:08 +0300

    hero cocktails => mocktails

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ const Hero = () => {
     <>
         <section id='hero' className='noisy'>
             <h1 className='title'>
-                Cocktails
+                Mocktails
             </h1>
             <img src="/images/hero-left-leaf.png" alt="left-leaf" className='left-leaf'/>
             <img src="/images/hero-right-leaf.png" alt="right-leaf" className='right-leaf'/>
@@ -79,7 +79,6 @@ const Hero = () => {
                             Sip the Spirit <br /> of Summer
                         </p>
                     </div>
-
                     <div className='view-cocktails'>
                         <p className="subtitle">
                             Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses.
